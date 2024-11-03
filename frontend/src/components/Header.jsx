2500eb79b2c62739aae1040fa85ff44543e8dd92
skywang1234 <32024922+skywang1234@users.noreply.github.com>
2024-11-03 00:24:50 -0400

    add slight highlights to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-        <header className="justify-center text-center text-4xl text-heliotrope-700 font-bodoni">
+        <header className="justify-center text-center text-4xl text-heliotrope-700 font-bodoni font-medium">
             Good Morning, User
         </header>
     );
