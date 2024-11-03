0d0d49a7fa807930ed814aeaec93e909c26a8b92
Hejin <hejin.wang@beepbeep.tech>
2024-11-03 00:10:12 -0400

    fe: login and register components and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./Landing";
 import About from "./About";
+import Login from "./components/Login/Login";
+import Register from "./components/Login/Register";
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <div>
       <Router>
           <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
               <Route path="/" element={<Landing />} />
               <Route path="/about" element={<About />} />
           </Routes>
